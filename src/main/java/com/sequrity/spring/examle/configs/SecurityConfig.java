db1ba03ce02861becb910a35f1a5351713963bdc
Sergey Skidanov <vonadiks@mail.ru>
2021-08-01 21:18:13 +0400

    commit before changes for homework

--- src/main/java/com/sequrity/spring/examle/configs/SecurityConfig.java
+++ src/main/java/com/sequrity/spring/examle/configs/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.sequrity.spring.examle.configs;
 
-import com.flamexander.spring.security.cookbook.dao.services.UserService;
+import com.sequrity.spring.examle.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -12,15 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        log.info("Dao Authentication Provider");
+        //log.info("Dao Authentication Provider");
         http.authorizeRequests()
                 .antMatchers("/auth_page/**").authenticated()
                 .antMatchers("/user_info").authenticated()
